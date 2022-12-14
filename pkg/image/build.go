428492adaf378ac4340c37deb0e76f62a1670d33
Gaurav Rao <wizgrao@gmail.com>
2022-12-13 19:57:49 -0800

    asdf

--- pkg/image/build.go
+++ pkg/image/build.go
@@ -1,14 +1,11 @@
 package image
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 
 	"github.com/sieve-data/cog/pkg/config"
 	"github.com/sieve-data/cog/pkg/docker"
 	"github.com/sieve-data/cog/pkg/dockerfile"
-	"github.com/sieve-data/cog/pkg/global"
 	"github.com/sieve-data/cog/pkg/util/console"
 )
 
@@ -37,40 +34,6 @@ func Build(cfg *config.Config, dir, imageName string, progressOutput string) err
 		return fmt.Errorf("Failed to build Docker image: %w", err)
 	}
 
-	console.Info("Adding labels to image...")
-	schema, err := GenerateOpenAPISchema(imageName, cfg.Build.GPU)
-	if err != nil {
-		return fmt.Errorf("Failed to get type signature: %w", err)
-	}
-	configJSON, err := json.Marshal(cfg)
-	if err != nil {
-		return fmt.Errorf("Failed to convert config to JSON: %w", err)
-	}
-	// We used to set the cog_version and config labels in Dockerfile, because we didn't require running the
-	// built image to get those. But, the escaping of JSON inside a label inside a Dockerfile was gnarly, and
-	// doesn't seem to be a problem here, so do it here instead.
-	labels := map[string]string{
-		global.LabelNamespace + "version": global.Version,
-		global.LabelNamespace + "config":  string(bytes.TrimSpace(configJSON)),
-		// Backwards compatibility. Remove for 1.0.
-		"org.cogmodel.deprecated":  "The org.cogmodel labels are deprecated. Use run.cog.",
-		"org.cogmodel.cog_version": global.Version,
-		"org.cogmodel.config":      string(bytes.TrimSpace(configJSON)),
-	}
-
-	// OpenAPI schema is not set if there is no predictor.
-	if len((*schema).(map[string]interface{})) != 0 {
-		schemaJSON, err := json.Marshal(schema)
-		if err != nil {
-			return fmt.Errorf("Failed to convert type signature to JSON: %w", err)
-		}
-		labels[global.LabelNamespace+"openapi_schema"] = string(schemaJSON)
-		labels["org.cogmodel.openapi_schema"] = string(schemaJSON)
-	}
-
-	if err := docker.BuildAddLabelsToImage(imageName, labels); err != nil {
-		return fmt.Errorf("Failed to add labels to image: %w", err)
-	}
 	return nil
 }
 
