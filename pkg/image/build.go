8461e0390d00a0caa39c4fbddb324222672ee9a3
abhi12-ayalur <abhinav.ayalur@gmail.com>
2023-06-20 21:07:50 +0000

    fix: remove cog openapi schema stuff

--- pkg/image/build.go
+++ pkg/image/build.go
@@ -1,13 +1,9 @@
 package image
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
 	"os"
-
-	"github.com/sieve-data/cog/pkg/global"
 
 	"github.com/sieve-data/cog/pkg/config"
 	"github.com/sieve-data/cog/pkg/docker"
@@ -44,38 +40,38 @@ func Build(cfg *config.Config, dir, imageName string, progressOutput string, wri
 	}
 
 	console.Info("Adding labels to image...")
-	schema, err := GenerateOpenAPISchema(imageName, cfg.Build.GPU)
-	if err != nil {
-		return "", fmt.Errorf("Failed to get type signature: %w", err)
-	}
-	configJSON, err := json.Marshal(cfg)
-	if err != nil {
-		return "", fmt.Errorf("Failed to convert config to JSON: %w", err)
-	}
-	// We used to set the cog_version and config labels in Dockerfile, because we didn't require running the
-	// built image to get those. But, the escaping of JSON inside a label inside a Dockerfile was gnarly, and
-	// doesn't seem to be a problem here, so do it here instead.
-	labels := map[string]string{
-		global.LabelNamespace + "version": global.Version,
-		global.LabelNamespace + "config":  string(bytes.TrimSpace(configJSON)),
-		// Mark the image as having an appropriate init entrypoint. We can use this
-		// to decide how/if to shim the image.
-		global.LabelNamespace + "has_init": "true",
-		// Backwards compatibility. Remove for 1.0.
-		"org.cogmodel.deprecated":  "The org.cogmodel labels are deprecated. Use run.cog.",
-		"org.cogmodel.cog_version": global.Version,
-		"org.cogmodel.config":      string(bytes.TrimSpace(configJSON)),
-	}
+	// schema, err := GenerateOpenAPISchema(imageName, cfg.Build.GPU)
+	// if err != nil {
+	// 	return "", fmt.Errorf("Failed to get type signature: %w", err)
+	// }
+	// configJSON, err := json.Marshal(cfg)
+	// if err != nil {
+	// 	return "", fmt.Errorf("Failed to convert config to JSON: %w", err)
+	// }
+	// // We used to set the cog_version and config labels in Dockerfile, because we didn't require running the
+	// // built image to get those. But, the escaping of JSON inside a label inside a Dockerfile was gnarly, and
+	// // doesn't seem to be a problem here, so do it here instead.
+	// labels := map[string]string{
+	// 	global.LabelNamespace + "version": global.Version,
+	// 	global.LabelNamespace + "config":  string(bytes.TrimSpace(configJSON)),
+	// 	// Mark the image as having an appropriate init entrypoint. We can use this
+	// 	// to decide how/if to shim the image.
+	// 	global.LabelNamespace + "has_init": "true",
+	// 	// Backwards compatibility. Remove for 1.0.
+	// 	"org.cogmodel.deprecated":  "The org.cogmodel labels are deprecated. Use run.cog.",
+	// 	"org.cogmodel.cog_version": global.Version,
+	// 	"org.cogmodel.config":      string(bytes.TrimSpace(configJSON)),
+	// }
 
-	// OpenAPI schema is not set if there is no predictor.
-	if len((*schema).(map[string]interface{})) != 0 {
-		schemaJSON, err := json.Marshal(schema)
-		if err != nil {
-			return "", fmt.Errorf("Failed to convert type signature to JSON: %w", err)
-		}
-		labels[global.LabelNamespace+"openapi_schema"] = string(schemaJSON)
-		labels["org.cogmodel.openapi_schema"] = string(schemaJSON)
-	}
+	// // OpenAPI schema is not set if there is no predictor.
+	// if len((*schema).(map[string]interface{})) != 0 {
+	// 	schemaJSON, err := json.Marshal(schema)
+	// 	if err != nil {
+	// 		return "", fmt.Errorf("Failed to convert type signature to JSON: %w", err)
+	// 	}
+	// 	labels[global.LabelNamespace+"openapi_schema"] = string(schemaJSON)
+	// 	labels["org.cogmodel.openapi_schema"] = string(schemaJSON)
+	// }
 
 	return dockerfileContents, nil
 }
@@ -115,38 +111,38 @@ func BuildFromGenerate(cfg *config.Config, dir, imageName string, progressOutput
 	}
 
 	console.Info("Adding labels to image...")
-	schema, err := GenerateOpenAPISchema(imageName, cfg.Build.GPU)
-	if err != nil {
-		return "", fmt.Errorf("Failed to get type signature: %w", err)
-	}
-	configJSON, err := json.Marshal(cfg)
-	if err != nil {
-		return "", fmt.Errorf("Failed to convert config to JSON: %w", err)
-	}
+	// schema, err := GenerateOpenAPISchema(imageName, cfg.Build.GPU)
+	// if err != nil {
+	// 	return "", fmt.Errorf("Failed to get type signature: %w", err)
+	// }
+	// configJSON, err := json.Marshal(cfg)
+	// if err != nil {
+	// 	return "", fmt.Errorf("Failed to convert config to JSON: %w", err)
+	// }
 	// We used to set the cog_version and config labels in Dockerfile, because we didn't require running the
 	// built image to get those. But, the escaping of JSON inside a label inside a Dockerfile was gnarly, and
 	// doesn't seem to be a problem here, so do it here instead.
-	labels := map[string]string{
-		global.LabelNamespace + "version": global.Version,
-		global.LabelNamespace + "config":  string(bytes.TrimSpace(configJSON)),
-		// Mark the image as having an appropriate init entrypoint. We can use this
-		// to decide how/if to shim the image.
-		global.LabelNamespace + "has_init": "true",
-		// Backwards compatibility. Remove for 1.0.
-		"org.cogmodel.deprecated":  "The org.cogmodel labels are deprecated. Use run.cog.",
-		"org.cogmodel.cog_version": global.Version,
-		"org.cogmodel.config":      string(bytes.TrimSpace(configJSON)),
-	}
+	// labels := map[string]string{
+	// 	global.LabelNamespace + "version": global.Version,
+	// 	global.LabelNamespace + "config":  string(bytes.TrimSpace(configJSON)),
+	// 	// Mark the image as having an appropriate init entrypoint. We can use this
+	// 	// to decide how/if to shim the image.
+	// 	global.LabelNamespace + "has_init": "true",
+	// 	// Backwards compatibility. Remove for 1.0.
+	// 	"org.cogmodel.deprecated":  "The org.cogmodel labels are deprecated. Use run.cog.",
+	// 	"org.cogmodel.cog_version": global.Version,
+	// 	"org.cogmodel.config":      string(bytes.TrimSpace(configJSON)),
+	// }
 
 	// OpenAPI schema is not set if there is no predictor.
-	if len((*schema).(map[string]interface{})) != 0 {
-		schemaJSON, err := json.Marshal(schema)
-		if err != nil {
-			return "", fmt.Errorf("Failed to convert type signature to JSON: %w", err)
-		}
-		labels[global.LabelNamespace+"openapi_schema"] = string(schemaJSON)
-		labels["org.cogmodel.openapi_schema"] = string(schemaJSON)
-	}
+	// if len((*schema).(map[string]interface{})) != 0 {
+	// 	schemaJSON, err := json.Marshal(schema)
+	// 	if err != nil {
+	// 		return "", fmt.Errorf("Failed to convert type signature to JSON: %w", err)
+	// 	}
+	// 	labels[global.LabelNamespace+"openapi_schema"] = string(schemaJSON)
+	// 	labels["org.cogmodel.openapi_schema"] = string(schemaJSON)
+	// }
 
 	return dockerfileContents, nil
 }
